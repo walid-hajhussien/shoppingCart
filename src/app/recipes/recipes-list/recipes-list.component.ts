bbaea4e1a2084b90c539baea74c21d94669698f8
walid-hajhussien <eng.waleed_com@yahoo.com>
2019-07-20 21:39:22 +0300

    (add) component communication

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  @Output()recipeWasSelected=new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'just a test recipe',
@@ -17,6 +18,10 @@ export class RecipesListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onItemSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
